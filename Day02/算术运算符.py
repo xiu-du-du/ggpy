ff53c964fb36f4d5bca24c27d0e48d7b49b3af42
xiuxiu <cnimmor@gmail.com>
2021-09-22 03:39:39 +0800

    log_2

---
+++ Day02/算术运算符.py
@@ -0,0 +1,36 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/22 3:28
+# @Author:xiu-du-du
+# @File:算术运算符
+# @Project:ggpy
+
+
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print(a*(a+b))
+
+# 字符串拼接
+a='123'
+b='456'
+print(a+b)
+
+# 字符串重复
+a='python'
+print(a*3)
+
+
+
+
+
+
+
+
+
+
