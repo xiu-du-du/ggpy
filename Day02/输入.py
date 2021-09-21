3954fd47c6dd33f08fafa765f82602c5b110676e
xiuxiu <cnimmor@gmail.com>
2021-09-22 04:49:37 +0800

    log_2

---
+++ Day02/输入.py
@@ -0,0 +1,24 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/22 4:38
+# @Author:xiu-du-du
+# @File:输入
+# @Project:ggpy
+
+# input()函数 --> 输入
+# input()函数返回值为字符串类型
+userName=input('请输入用户名：')
+userPassword=input('请输入密码：')
+print('你输入的用户名为%s,密码为%s' % (userName,userPassword))
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day02/输出.py
@@ -0,0 +1,14 @@
+# 普通输出
+print('Hello,Python!')
+
+# 格式化输出
+# %s --> 字符串
+# %d --> 数值
+name='Tom'
+age=18
+print('我的名字是%s,我的年龄是%d' % (name,age))
+
+# 占位符
+name='Tom'
+age=18
+print(f'我的名字是{name},我的年龄是{age}')
