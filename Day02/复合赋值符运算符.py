fb44095520758b51e5e71bdb7b1fdc4b32641915
xiuxiu <cnimmor@gmail.com>
2021-09-22 03:43:41 +0800

    log_2

---
+++ Day02/复合赋值符运算符.py
@@ -0,0 +1,5 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/22 3:43
+# @Author:xiu-du-du
+# @File:复合赋值符运算符
+# @Project:ggpy
---
+++ Day02/赋值运算符.py
@@ -0,0 +1,25 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/22 3:40
+# @Author:xiu-du-du
+# @File:赋值运算符
+# @Project:ggpy
+
+a=5
+print(a)
+a=b=10
+print(a,b)
+a,b,c=1,9,6
+print(a,b,c)
+
+
+
+
+
+
+
+
+
+
+
+
+
