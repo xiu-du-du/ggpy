8671620abd122cc8f259f450990ecbc7d344937d
xiuxiu <cnimmor@gmail.com>
2021-09-23 11:15:00 +0800

    log_3

---
+++ Day04/元祖高级.py
@@ -0,0 +1,31 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/23 10:55
+# @Author:xiu-du-du
+# @File:元祖高级
+# @Project:ggpy
+
+# 元祖的访问
+nameTuple=('tom','jin','mary')
+print(type(nameTuple))
+print(nameTuple)
+
+# 修改元祖
+# 元祖是不允许修改的
+# nameTuple[1]='tea'
+# print(nameTuple)
+
+# 定义只有一个值的元组
+# 在元组中的元素后加一个逗号
+tTuple=(5,)
+print(type(tTuple))
+print(tTuple)
+
+
+
+
+
+
+
+
+
+
