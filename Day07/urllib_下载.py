3bb9fb90d8d2653d6a46a1f9bae953faa5cd1a1c
xiuxiu <cnimmor@gmail.com>
2021-09-25 07:07:05 +0800

    log_7

---
+++ Day07/urllib_下载.py
@@ -0,0 +1,24 @@
+# _*_ coding : utf-8 _*_
+# @Time:2021/9/25 7:00
+# @Author:xiu-du-du
+# @File:urllib_下载
+# @Project:ggpy
+
+import urllib.request
+
+# 下载
+# urllib.request.urlretrieve(下载地址，下载文件名)
+
+# 网页下载
+# url='http://www.baidu.com'
+# urllib.request.urlretrieve(url,'bd.html')
+
+# 图片下载
+# url='https://img1.baidu.com/it/u=2698517060,764350945&fm=26&fmt=auto'
+# urllib.request.urlretrieve(url,'lisa.png')
+
+# 视频下载
+# url='https://vd2.bdstatic.com/mda-middxdb8abwcx819/fhd/cae_h264_nowatermark/1631613500608608789/mda-middxdb8abwcx819.mp4?v_from_s=hkapp-haokan-nanjing&auth_key=1632526489-0-0-bcecd97644d9e7a18b41c19bc7959358&bcevod_channel=searchbox_feed&pd=1&pt=3&abtest='
+# urllib.request.urlretrieve(url,'lisa.mp4')
+
+
