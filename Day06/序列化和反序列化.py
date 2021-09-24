a75f007eb2f59042397bfe27ed040a97084915f3
xiuxiu <cnimmor@gmail.com>
2021-09-24 11:43:21 +0800

    log_6

--- Day06/序列化和反序列化.py
+++ Day06/序列化和反序列化.py
@@ -3,3 +3,30 @@
 # @Author:xiu-du-du
 # @File:序列化和反序列化
 # @Project:ggpy
+import json
+# dumps(对象名)  序列化
+# newFile=open(r'demo\save.txt','w')
+# nameList=['zs','ls']
+# newStr=json.dumps(nameList)
+# newFile.write(newStr)
+# newFile.close()
+
+# dump(对象名,文件名)  序列化
+# newFile=open(r'demo\save1.txt','w')
+# nameList=['zs','ls']
+# json.dump(nameList,newFile)
+# newFile.close()
+
+# loads(对象名) 反序列化
+# readFile=open(r'demo\save1.txt','r')
+# readCount=readFile.read()
+# print(type(json.loads(readCount)))
+# print(json.loads(readCount))
+# readFile.close()
+
+# load(文件名) 反序列化
+readFile=open(r'demo\save1.txt','r')
+res=json.load(readFile)
+print(type(res))
+print(res)
+readFile.close()
